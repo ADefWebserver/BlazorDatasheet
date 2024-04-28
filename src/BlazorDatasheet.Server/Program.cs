4aeb7074fd8b9ca23679a2c4f88df409acd390c5
Michael Washington <webmaster@adefwebserver.com>
2024-04-28 12:42:14 -0700

    Adding Radzen and Toolbar layout

--- src/BlazorDatasheet.Server/Program.cs
+++ src/BlazorDatasheet.Server/Program.cs
@@ -1,9 +1,11 @@
+using Radzen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddRadzenComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
